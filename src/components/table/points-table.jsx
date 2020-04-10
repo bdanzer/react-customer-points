207608b97f0ccb06c7111b1a4e67923d5e785886
bdanzer <britt.danzer@gmail.com>
2020-04-10 16:05:36 -0500

    fixed mobile for some bit of responsiveness

--- src/components/table/points-table.jsx
+++ src/components/table/points-table.jsx
@@ -4,15 +4,17 @@ import "./points-table.scss";
 
 export default function PointsTable({ children, data, headers }) {
     return (
-        <table id="customers">
-            <tbody>
-                <tr>
-                    {headers.map((header) => (
-                        <th>{header}</th>
-                    ))}
-                </tr>
-                {Object.keys(data).map((key, i) => children(data[key], i))}
-            </tbody>
-        </table>
+        <div className="table-wrap">
+            <table id="customers">
+                <tbody>
+                    <tr>
+                        {headers.map((header) => (
+                            <th>{header}</th>
+                        ))}
+                    </tr>
+                    {Object.keys(data).map((key, i) => children(data[key], i))}
+                </tbody>
+            </table>
+        </div>
     );
 }
