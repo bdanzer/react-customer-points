763b1cea0d576f2fb32ecacc2359143b6bd98eb2
bdanzer <britt.danzer@gmail.com>
2020-04-10 15:41:02 -0500

    Updates

---
+++ precache-manifest.58c2f670a21c88b447ecdce75d99b355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c76fb0c8d0cd560fdc4f7d17057219",
+    "url": "/react-customer-points/index.html"
+  },
+  {
+    "revision": "23e708cbac1e959a2fc1",
+    "url": "/react-customer-points/static/css/main.0a48fe0a.chunk.css"
+  },
+  {
+    "revision": "a10e61ded7728847a3b9",
+    "url": "/react-customer-points/static/js/2.2eeb3f18.chunk.js"
+  },
+  {
+    "revision": "23e708cbac1e959a2fc1",
+    "url": "/react-customer-points/static/js/main.be826b4c.chunk.js"
+  },
+  {
+    "revision": "09dd1ae80e275621f6ec",
+    "url": "/react-customer-points/static/js/runtime~main.dca91846.js"
+  }
+]);
